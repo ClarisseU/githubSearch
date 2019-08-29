3bf5fa59a1b07a53e9b3eb751a52300edea49b72
Clarisse U <clarisseum@gmail.com>
2019-08-29 17:30:09 +0200

    add get profile info function

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private username: string;
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+    console.log("service is ready");
+   }
 }
