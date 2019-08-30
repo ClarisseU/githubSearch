f7b6786a1251958dab77aa687985453b3247e320
Clarisse U <clarisseum@gmail.com>
2019-08-30 10:31:07 +0200

    add api in environment

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -6,8 +6,15 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProfileService {
   private username: string;
+  private access_token='af44cfda8e93f3935e2b4122c17d093072a467d3';
 
   constructor(private http:HttpClient) {
     console.log("service is ready");
+    this.username="ClarisseU";
+    this.access_token ="af44cfda8e93f3935e2b4122c17d093072a467d3";
+    
+   }
+   getInfoProfile(){
+     return this.http.get("https://api.github.com/users/" +this.username+"?access_token"+this.access_token);
    }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'af44cfda8e93f3935e2b4122c17d093072a467d3'
 };
 
 /*
