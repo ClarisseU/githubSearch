49b0073ce27f6bde7edc25d8f6af7a3b1632a09f
Clarisse U <clarisseum@gmail.com>
2019-08-30 17:16:41 +0200

    add repositories

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
   private username: string;
-  private access_token='af44cfda8e93f3935e2b4122c17d093072a467d3';
+  private access_token=environment.apiKey;
 
   constructor(private http:HttpClient) {
     console.log("service is ready");
     this.username="ClarisseU";
-    this.access_token ="af44cfda8e93f3935e2b4122c17d093072a467d3";
+    this.access_token=environment.apiKey;
     
    }
-   getInfoProfile(){
+  
+  getInfoProfile(){
      return this.http.get("https://api.github.com/users/" +this.username+"?access_token"+this.access_token);
    }
+   getRepo(){
+    return this.http.get("https://api.github.com/users/" +this.username+"/repos?access_token"+this.access_token);
+   }
+   
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../profile.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
@@ -7,13 +8,18 @@ import { ProfileService } from '../profile.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
+ profile: any;
+ repos: any;
   constructor(private proservice:ProfileService) { 
     this.proservice.getInfoProfile().subscribe(profile=>{
      console.log(profile);
+     this.profile = profile;
     });
+    this.proservice.getRepo().subscribe(repos=>{
+      console.log(repos);
+      this.repos = repos;
+    })
   }
-
   ngOnInit() {
   }
 
